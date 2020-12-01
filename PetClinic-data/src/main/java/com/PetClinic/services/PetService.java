2b065f8be360029fc2ed9b8895aa8761af96c150
Ayanda-Kubz <ayandaujkubeka@gmail.com>
2020-12-01 10:01:26 +0200

    Pet Clinic Web and Data.

---
+++ PetClinic-data/src/main/java/com/PetClinic/services/PetService.java
@@ -0,0 +1,14 @@
+package com.PetClinic.services;
+
+import com.PetClinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet owner);
+
+    Set<Pet> findAll();
+}
