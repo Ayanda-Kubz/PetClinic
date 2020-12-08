1fcd648f1d251a6ea913164c3d508ecc60d74f80
Ayanda-Kubz <ayandaujkubeka@gmail.com>
2020-12-08 10:32:11 +0200

    Added owners and updated controller to index page

--- PetClinic-web/src/main/java/com/PetClinic/controllers/OwnerController.java
+++ PetClinic-web/src/main/java/com/PetClinic/controllers/OwnerController.java
@@ -1,14 +1,24 @@
 package com.PetClinic.controllers;
 
+import com.PetClinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OwnerController {
 
-    @RequestMapping({"/owner", "/owner/index.hmtl"})
-    public String listOwner(){
+    private final OwnerService ownerService;
 
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+
+    @RequestMapping({"/owner", "/owner/index.hmtl"})
+    public String listOwner(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
         return "owner/index";
     }
 }
