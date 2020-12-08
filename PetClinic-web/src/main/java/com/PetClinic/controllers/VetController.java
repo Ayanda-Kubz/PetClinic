3bd848cbf81f8f42637c18fd3454fafc9fa353c4
Ayanda-Kubz <ayandaujkubeka@gmail.com>
2020-12-08 11:20:57 +0200

    Added vets and updated controller to index page

--- PetClinic-web/src/main/java/com/PetClinic/controllers/VetController.java
+++ PetClinic-web/src/main/java/com/PetClinic/controllers/VetController.java
@@ -1,14 +1,23 @@
 package com.PetClinic.controllers;
 
+import com.PetClinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vet", "/vet/index.hmtl"})
-    public String listVets(){
+    private final VetService vetService;
 
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/vet", "/vet/index.hmtl"})
+    public String listVets(Model model){
+
+        model.addAttribute("vets",vetService.findAll());
         return "vet/index";
     }
 }
