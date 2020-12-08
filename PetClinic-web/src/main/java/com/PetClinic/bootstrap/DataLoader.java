e4b4a31c54d3eff0d2d6f44ba1144f34f84ca689
Ayanda-Kubz <ayandaujkubeka@gmail.com>
2020-12-08 12:11:06 +0200

    Added vets and updated controller to index page

--- PetClinic-web/src/main/java/com/PetClinic/bootstrap/DataLoader.java
+++ PetClinic-web/src/main/java/com/PetClinic/bootstrap/DataLoader.java
@@ -24,13 +24,11 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Ayanda");
         owner1.setLastName("Kubeka");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glen");
         ownerService.save(owner2);
@@ -38,13 +36,11 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded  owners ....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Axe");
         vet1.setLastName("Roy");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("James");
         vet2.setLastName("Smaart");
         vetService.save(vet2);
