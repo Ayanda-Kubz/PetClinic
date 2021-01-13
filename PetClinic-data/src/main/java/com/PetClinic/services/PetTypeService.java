9d660bf5c592069c0b6f6e284db5d65a8616208b
Ayanda-Kubz <ayandaujkubeka@gmail.com>
2021-01-13 15:42:04 +0200

    Creating Pet type Map class, Service class and loading data for the pet types

---
+++ PetClinic-data/src/main/java/com/PetClinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.PetClinic.services;
+
+import com.PetClinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long> {
+}
--- PetClinic-web/src/main/java/com/PetClinic/bootstrap/DataLoader.java
+++ PetClinic-web/src/main/java/com/PetClinic/bootstrap/DataLoader.java
@@ -1,5 +1,7 @@
 package com.PetClinic.bootstrap;
 
+import com.PetClinic.model.PetType;
+import com.PetClinic.services.PetTypeService;
 import com.PetClinic.services.map.OwnerServiceMap;
 import com.PetClinic.services.map.VetServiceMap;
 import com.PetClinic.model.Owner;
@@ -14,14 +16,25 @@ public class DataLoader implements CommandLineRunner {
 
     private OwnerService ownerService;
     private VetService vetService;
+    private PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
+
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType saveDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType saveCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Ayanda");
